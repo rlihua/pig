43b11acdb7511e633948e45ce646f966db6db0fa
rlihua <1185832898@qq.com>
2019-11-09 16:12:28 +0800

    add

---
+++ index.php
@@ -0,0 +1,244 @@
+<!doctype html>
+<html>
+<head>
+    <meta http-equiv="Expires" content="0">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Cache-control" content="no-cache">
+    <meta http-equiv="Cache" content="no-cache">
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta name=viewport content="width=device-width,initial-scale=1,maximum-scale=1">
+    <meta name=format-detection content="telephone=no">
+    <meta name=apple-mobile-web-app-capable content=yes>
+    <meta name=apple-mobile-web-app-status-bar-style content=black>
+	<title>小猪智慧店铺</title>
+    <link rel="stylesheet" href="./css/weui.css"/>
+    <link rel="stylesheet" href="./css/swiper.min.css"/>
+    <link rel="stylesheet" href="./css/index.css"/>
+</head>
+<body ontouchstart>
+    <div class="model-body">
+        <div class="model-first-div model-each-div">
+            <div class="weui-loadmore weui-loadmore_line">
+                <span class="weui-loadmore__tips bold model-header-span">九大专属功能，营销一步就位</span>
+                <p class="model-header-p">小猪微店自动同步商品信息到手机店铺，打通门店会员与微信粉丝数据，九大专属会员营销功能。</p>
+            </div>
+            <div class="weui-flex">
+                <div class="weui-flex__item tcenter">
+                        <div class="placeholder bold"><img src="./img/pig_11.png">会员管理</div>
+                </div>
+                <div class="weui-flex__item tcenter">
+                        <div class="placeholder bold"><img src="./img/pig_12.png">会员积分</div>
+                </div>
+                <div class="weui-flex__item tcenter">
+                        <div class="placeholder bold"><img src="./img/pig_13.png">会员卡券</div>
+                </div>
+            </div>
+            <div class="weui-flex">
+                <div class="weui-flex__item tcenter">
+                        <div class="placeholder bold"><img src="./img/pig_14.png">会员储值</div>
+                </div>
+                <div class="weui-flex__item tcenter">
+                        <div class="placeholder bold"><img src="./img/pig_15.png">会员支付</div>
+                </div>
+                <div class="weui-flex__item tcenter">
+                        <div class="placeholder bold"><img src="./img/pig_16.png">会员拓客</div>
+                </div>
+            </div>
+            <div class="weui-flex">
+                <div class="weui-flex__item tcenter">
+                        <div class="placeholder bold"><img src="./img/pig_17.png">会员电商</div>
+                </div>
+                <div class="weui-flex__item tcenter">
+                        <div class="placeholder bold"><img src="./img/pig_18.png">会员预约</div>
+                </div>
+                <div class="weui-flex__item tcenter">
+                        <div class="placeholder bold"><img src="./img/pig_19.png">会员游戏</div>
+                </div>
+            </div>
+        </div>
+        <div class="model-two-div model-each-div">
+            <div class="weui-loadmore weui-loadmore_line">
+                <span class="weui-loadmore__tips bold model-header-span">六大应用场景</span>
+            </div>
+            <div class="weui-flex">
+                <div class="weui-flex__item tcenter">
+                        <div class="placeholder">
+                            <span class="weui-badge bold" style="margin-left: 5px;">1</span>
+                            <p class="yycj-one bold">微信公众号</p>
+                            <p class="yycj-two">信息推送、业务介绍等</p>
+                        </div>
+                </div>
+                <div class="weui-flex__item tcenter">
+                    <div class="placeholder">
+                        <span class="weui-badge bold" style="margin-left: 5px;">2</span>
+                        <p class="yycj-one bold">小程序</p>
+                        <p class="yycj-two">产品介绍、展示宣传、扫码支付等</p>
+                    </div>
+                </div>
+                <div class="weui-flex__item tcenter">
+                    <div class="placeholder">
+                        <span class="weui-badge bold" style="margin-left: 5px;">3</span>
+                        <p class="yycj-one bold">微信卡包</p>
+                        <p class="yycj-two">会员中心、会员积分、优惠活动等</p>
+                    </div>
+                </div>
+            </div>
+            <div class="weui-flex">
+                <div class="weui-flex__item tcenter">
+                        <div class="placeholder">
+                            <span class="weui-badge bold" style="margin-left: 5px;">4</span>
+                            <p class="yycj-one bold">员工拓客</p>
+                            <p class="yycj-two">工牌扫码、维护客户、绩效考核等</p>
+                        </div>
+                </div>
+                <div class="weui-flex__item tcenter">
+                    <div class="placeholder">
+                        <span class="weui-badge bold" style="margin-left: 5px;">5</span>
+                        <p class="yycj-one bold">店内场景</p>
+                        <p class="yycj-two">水牌、海报、桌贴等</p>
+                    </div>
+                </div>
+                <div class="weui-flex__item tcenter">
+                    <div class="placeholder">
+                        <span class="weui-badge bold" style="margin-left: 5px;">6</span>
+                        <p class="yycj-one bold">会员支付</p>
+                        <p class="yycj-two">支付后营销、快速支付通道等</p>
+                    </div>
+                </div>
+            </div>
+            <div class="weui-loadmore weui-loadmore_line">
+                <span class="weui-loadmore__tips bold model-header-span">全流程解决方案</span>
+                <p class="model-header-p">会员制消费转型升级全流程解决方案 体系化会员营销管理策略</p>
+            </div>
+            <img src="./img/qlc.png" alt="" class="qlc-img">
+        </div>
+        <div class="model-three-div model-each-div">
+            <div class="weui-loadmore weui-loadmore_line">
+                <span class="weui-loadmore__tips bold model-header-span">以提升每个门店营销力为使命！</span>
+                <p class="model-header-p">我们更关注产品落地和门店互联网综合解决方案。</p>
+            </div>
+            <div class="weui-flex model-three-flex">
+                <div class="weui-flex__item ">
+                    <div class="placeholder">
+                       <img src="./img/pig_21.png">
+                        一站式商家互联网+服务平台
+                    </div>
+                </div>
+                <div class="weui-flex__item tcenter">
+                    <div class="placeholder">
+                        <img src="./img/pig_22.png">
+                        门店在用
+                    </div>
+                </div>
+                <div class="weui-flex__item ">
+                    <div class="placeholder">
+                        <img src="./img/pig_23.png">
+                        平台客户
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="model-four-div model-each-div">
+            <div class="weui-loadmore weui-loadmore_line">
+                <span class="weui-loadmore__tips bold model-header-span">我们的案例</span>
+            </div>
+            <div class="swiper-container">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <div class="swiper-each-slide">
+                            <div class="weui-flex">
+                                <div class="weui-flex__item tcenter model-four-each-div">
+                                    <img src="./img/case-img.png">
+                                </div>
+                                <div class="weui-flex__item tcenter model-four-each-div">
+                                    <img src="./img/case-img.png">
+                                </div>
+                                <div class="weui-flex__item tcenter model-four-each-div">
+                                    <img src="./img/case-img.png">
+                                </div>
+                                <div class="weui-flex__item tcenter model-four-each-div">
+                                    <img src="./img/case-img.png">
+                                </div>
+                            </div>
+                            <div class="weui-flex">
+                                <div class="weui-flex__item tcenter model-four-each-div">
+                                    <img src="./img/case-img.png">
+                                </div>
+                                <div class="weui-flex__item tcenter model-four-each-div">
+                                    <img src="./img/case-img.png">
+                                </div>
+                                <div class="weui-flex__item tcenter model-four-each-div">
+                                    <img src="./img/case-img.png">
+                                </div>
+                                <div class="weui-flex__item tcenter model-four-each-div">
+                                    <img src="./img/case-img.png">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="swiper-each-slide">
+                            <div class="weui-flex">
+                                <div class="weui-flex__item tcenter model-four-each-div">
+                                    <img src="./img/case-img.png">
+                                </div>
+                                <div class="weui-flex__item tcenter model-four-each-div">
+                                    <img src="./img/case-img.png">
+                                </div>
+                                <div class="weui-flex__item tcenter model-four-each-div">
+                                    <img src="./img/case-img.png">
+                                </div>
+                                <div class="weui-flex__item tcenter model-four-each-div">
+                                    <img src="./img/case-img.png">
+                                </div>
+                            </div>
+                            <div class="weui-flex">
+                                <div class="weui-flex__item tcenter model-four-each-div">
+                                    <img src="./img/case-img.png">
+                                </div>
+                                <div class="weui-flex__item tcenter model-four-each-div">
+                                    <img src="./img/case-img.png">
+                                </div>
+                                <div class="weui-flex__item tcenter model-four-each-div">
+                                    <img src="./img/case-img.png">
+                                </div>
+                                <div class="weui-flex__item tcenter model-four-each-div">
+                                    <img src="./img/case-img.png">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-pagination"></div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-div tcenter">
+        <div class="footer-p tcenter">
+            <div class="footer-one-p bold">
+                正寻找一款更强大的门店营销管理系统？
+            </div>
+            <div class="footer-two-p">
+                <div class="tcenter"> 想要了解更多小猪解决方案？</div>
+                <div class="tcenter"> 看看我们如何帮您提升业绩</div>
+            </div>
+        </div>
+        <img src="./img/footer-img.png" class="footer-img">
+    </div>
+<script src="./js/swiper.min.js"></script>
+<script>
+    var swiper = new Swiper('.swiper-container', {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        }
+    });
+</script>
+</body>
+</html>
